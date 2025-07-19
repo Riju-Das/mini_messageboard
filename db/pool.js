40a2ec2faf1efe88b11af6ed353cea64221582bb
Riju Das <riju.das1910@gmail.com>
2025-07-19 20:01:54 +0530

    Commited

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg")
 
 module.exports = new Pool ({
-    connectionString: "postgresql://pdzdatabase_user:qmIRu6Zi1egqEqa1kohdc3HVQb3RUugx@dpg-d1tnokruibrs73fms3a0-a.oregon-postgres.render.com/pdzdatabase"
+    connectionString: "postgresql://pdzdatabase_user:qmIRu6Zi1egqEqa1kohdc3HVQb3RUugx@dpg-d1tnokruibrs73fms3a0-a/pdzdatabase"
 })
